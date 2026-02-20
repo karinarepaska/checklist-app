f9c889ac2a0d9b0e00922ad211ffe4c8d3c59a30
karinarepaska <68991426+karinarepaska@users.noreply.github.com>
2026-02-20 22:53:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
-self.addEventListener("install", event => {
+const CACHE = "checklist-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(clients.claim());
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
 });
